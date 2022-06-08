0d6b3e563f7d4f80621baa6e51a15fe2047c5fdd
Marcus ParkerGarruba <mparkergarruba@gmail.com>
2022-06-08 08:57:43 -0400

    commit tree a4c6ec51566733c5168a0414cbb01a6a44e9a466

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (pickup) {
+    let hq = 42
+    return Math.abs(pickup - hq)
+}
+function distanceFromHqInFeet (pickup) {
+    return distanceFromHqInBlocks(pickup) * 264
+}
+function distanceTravelledInFeet (pickupBlock, dropoffBlock) {
+    return Math.abs(pickupBlock - dropoffBlock) * 264
+}
+function calculatesFarePrice (start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0
+    } else if (distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start,destination) - 400) * 0.02
+    } else if (distanceTravelledInFeet(start,destination) < 2500) {
+        return 25
+    } else return "cannot travel that far" 
+}
